8fc16e72bbf69249372de8811dc2e83c4b94bf22
Тараруева Юлия Николаевна <yuliya.tararueva@rtlabs.ru>
2024-02-18 23:16:14 +0800

    add reactive approach

--- src/main/java/com/exam/task2/service/HandlerService.java
+++ src/main/java/com/exam/task2/service/HandlerService.java
@@ -5,6 +5,7 @@ import com.exam.task2.model.Event;
 import com.exam.task2.model.Result;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
 
 import java.time.Duration;
 import java.util.List;
@@ -32,25 +33,27 @@ public class HandlerService implements Handler {
 
     @Override
     public void performOperation() {
+        Flux<Event> eventFlux = Flux.create(fluxSink -> client.readData());
+        eventFlux.doOnNext(event -> {
+                            List<CompletableFuture<Result>> cfs = event.recipients().stream()
+                                    .map(address -> CompletableFuture.supplyAsync(() -> client.sendData(address, event.payload()), executor)
+                                            .thenApply(result -> {
+                                                if (result.equals(Result.REJECTED)) {
+                                                    try {
+                                                        Thread.sleep(timeout().toMillis());
+                                                    } catch (InterruptedException e) {
+                                                        throw new RuntimeException(e);
+                                                    }
+                                                    return client.sendData(address, event.payload());
+                                                } else {
+                                                    return result;
+                                                }
+                                            }))
+                                    .toList();
 
-        Event event = client.readData();
-
-        List<CompletableFuture<Result>> cfs = event.recipients().stream()
-                .map(address -> CompletableFuture.supplyAsync(() -> client.sendData(address, event.payload()), executor)
-                        .thenApply(result -> {
-                            if (result.equals(Result.REJECTED)) {
-                                try {
-                                    Thread.sleep(timeout().toMillis());
-                                } catch (InterruptedException e) {
-                                    throw new RuntimeException(e);
-                                }
-                                return client.sendData(address, event.payload());
-                            } else {
-                                return result;
-                            }
-                        }))
-                .toList();
-
-        cfs.stream().map(CompletableFuture::join).collect(Collectors.toList());
+                            cfs.stream().map(CompletableFuture::join).collect(Collectors.toList());
+                        }
+                )
+                .subscribe();
     }
 }
